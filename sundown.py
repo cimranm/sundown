1f0027778c378282d0f8b73d3be89957687561ff
Cam <cameronmcmenamie@gmail.com>
2021-09-01 22:46:17 +1000

    Working

--- sundown.py
+++ sundown.py
@@ -1,11 +1,45 @@
 #! /usr/bin/env python3
 
-import datetime
+from datetime import date
 from suntime import Sun, SunTimeException
 
-import location 
 
-location.get_location()
+import location 
+from  geopy.geocoders import Nominatim
+
+
+
+geolocator = Nominatim(user_agent="http")
+
+country     = "Australia"
+city        = "Sydney"
+
+
+
+loc = geolocator.geocode (f'{city},{country}')
+
+lat  = loc.latitude
+long = loc.longitude
+
+# Suntime
+
+sun = Sun(lat, long)
+
+date = date.today()
+
+
+
+risetime    = sun.get_local_sunrise_time(date)
+settime     = sun.get_local_sunset_time(date)
+
+print (f"RISE: {risetime.strftime('%H:%M')}")
+
+print (f"SET: {settime.strftime('%H:%M')}")
+#settime     = sun.get_sunset
+
+
+
+
 
 
 
