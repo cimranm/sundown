8e2113dcd58f494d1cb39e915746362dac187d88
Cam <cameronmcmenamie@gmail.com>
2021-09-02 21:40:24 +1000

    Add comment of filename

--- location.py
+++ location.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+# location.py
+
 from  geopy.geocoders import Nominatim
 
 def get_location():
--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 
+
+# Install requirements
 pip3 install geopy      > /dev/null
 pip3 install suntime    > /dev/null
